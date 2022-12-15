fa1bf42c552f17be1141acc9f775545803b01902
jihad <jihadrifi909@gmail.com>
2022-12-15 10:44:32 +0100

    add sleep

--- CountDown.java
+++ CountDown.java
@@ -1,9 +1,10 @@
 class CountDown {
 
-	public static void main(String[] args) {
+	public static void main ( String [ ] args ) throws InterruptedException {
 	
 		for (int i = 10; i >= 0; i--) {
 			System.out.println(i);
+			Thread.sleep(1000);
 		}
 		
 	}
